f6bdb70a1a13b9ef85049845e7db9e9a5aeec86e
Eryk Majoch <emajoch1@sheffield.ac.uk>
2024-06-10 20:15:29 +0100

    Changed Jai to Zig

--- random_language.py
+++ random_language.py
@@ -3,7 +3,7 @@ import random
 def main() -> None:
     languages: list[str] = [
         "C", "C++", "C#", "Python", "Rust", "Java", "Ruby", "Javascript",
-        "Haskell", "Go", "Julia", "Jai"
+        "Haskell", "Go", "Julia", "Zig"
     ]
 
     print(f"Your random language for this challenge is {random.choice(languages)}!")
