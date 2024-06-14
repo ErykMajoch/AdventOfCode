cb48fa082cee2990e96a246cf572b7e7000642a6
Eryk Majoch <emajoch1@sheffield.ac.uk>
2024-06-14 23:27:40 +0100

    Removed zig, added shell and lua

--- random_language.py
+++ random_language.py
@@ -3,7 +3,7 @@ import random
 def main() -> None:
     languages: list[str] = [
         "C", "C++", "C#", "Python", "Rust", "Java", "Ruby", "Javascript",
-        "Haskell", "Go", "Julia", "Zig"
+        "Haskell", "Go", "Julia", "Shell", "Lua"
     ]
 
     print(f"Your random language for this challenge is {random.choice(languages)}!")
